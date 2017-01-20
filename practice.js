3aaa49b673a0d91ab3ddc5e55c0ac4b738ebf2e3
Brennan Boman <brennanboman@Brennans-MacBook-Air.local>
2017-01-19 19:54:34 -0700

    first few done

--- practice.js
+++ practice.js
@@ -3,8 +3,11 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
-
+var me = {
+  name: "Brennan",
+  age: 29
+} //Code here
+alert(me.name)
 
 
 
@@ -15,17 +18,31 @@
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+var favoriteThings = {
+  band: "Soilwork",
+  food: "Pizza",
+  person: "Shoni",
+  book: "Inheritance Cycle",
+  movie: "Goonies",
+  holiday: "Thanksgiving"
+}  //Code here
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+favoriteThings["car"] = "Saturn";
+favoriteThings["brand"] = "KREW";
+
+ //Code here
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
+
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
+
+//Code here
 
 
 
